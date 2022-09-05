91dfa18626074438a04632bce4b52e07e70269ac
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-09-05 10:28:29 +0800

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>NateChain</title>
+      </Head>
       <Image src="/logo.svg" alt="Logo" width={512} height={512} />
     </>
   );
